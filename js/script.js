bafeebd75e6f8e77ee3e35df45db37732784aefb
dolores <loliiipineyro@gmail.com>
2022-10-03 22:33:13 -0300

    codeando el proyecto puro js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,87 @@
-const Clickbutton = document.querySelectorAll('.button')
-let cart = []
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modalContainer");
 
-Clickbutton.forEach(btn => {
-    btn.addEventListener('click', addToCart)
-})
+const products = [
+    {
+        id: 1,
+        nombre: "Balenciaga Hourglass Bag",
+        precio: 1450,
+        img: "../fts/balenciaga.jpg", 
+    },
+    {
+        id: 2,
+        nombre: "Hermès Birkin Bag",
+        precio: 11414,
+        img: "../fts/birkin.jpg",
+    },
+    {
+        id: 3,
+        nombre: "Channel 22.5 Bag",
+        precio: 3360,
+        img: "../fts/chanel.jpg",
+    },
+    {
+        id: 4,
+        nombre: "Louis Vuitton Twist-MM Bag",
+        precio: 3450,
+        img: "../fts/louisvuitton.jpg",
+    },
+    {
+        id: 5,
+        nombre: "Prada Cleo Bag",
+        precio: 1950,
+        img: "../fts/prada.jpg",
+    },
+    {
+        id: 6,
+        nombre: "YSL Kaia Bag",
+        precio: 1450,
+        img: "../fts/ysl.jpg",
+    },
+];
 
-function addToCartItem(e) {
-    const button = e.target
-    const item = button.closest('.card')
-    const itemTitle = item.querySelector('.card-title').textContent;
-    const itemPrice = item.querySelector('.precio').textContent; 
-    const itemImage = item.querySelector('.card-img.top').src;
+let carrito = [];
 
-    const newCart = {
-        title: itemTitle,
-        precio: itemPrice,
-        img: itemImage,
-        cantidad: 1 
-    }
+products.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="price">${product.precio} $</p>
+    `;
 
-    addItemCart(newCart)
-}
+    shopContent.append(content); 
 
-function addItemCart(newCart){
-    cart.push(newCart)
-}
+    let buy = document.createElement("button");
+    buy.innerText = "add to cart";
+    buy.className = "card-button";
+
+    content.append(buy);
+
+    buy.addEventListener("click", () => {
+        carrito.push({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+        });
+        console.log(carrito);
+    });
+});
+
+verCarrito.addEventListener("click", () => {
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modalHeader";
+    modalHeader.innerHTML = `
+        <h1 class="modal-header-title">CART</h1>
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalButton = document.createElement("h1");
+    modalButton.innerText = "X";
+    modalButton.className = "modal-header-button";
+
+    modalHeader.append(modalButton)
+});
