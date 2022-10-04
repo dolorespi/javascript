4493ab13ffc79025df1e8612ada2999e754bbfb2
dolores <loliiipineyro@gmail.com>
2022-10-03 23:14:19 -0300

    terminando por hoy

--- js/script.js
+++ js/script.js
@@ -72,8 +72,10 @@ products.forEach((product) => {
 });
 
 verCarrito.addEventListener("click", () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
     const modalHeader = document.createElement("div");
-    modalHeader.className = "modalHeader";
+    modalHeader.className = "modal-header";
     modalHeader.innerHTML = `
         <h1 class="modal-header-title">CART</h1>
     `;
@@ -83,5 +85,28 @@ verCarrito.addEventListener("click", () => {
     modalButton.innerText = "X";
     modalButton.className = "modal-header-button";
 
-    modalHeader.append(modalButton)
+    modalButton.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    })
+
+    modalHeader.append(modalButton);
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div");
+        carritoContent.className = "modal-content";
+        carritoContent.innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p>${product.precio} $</p>
+    `;
+
+    modalContainer.append(carritoContent)
+    });
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+
+    const totalBuying = document.createElement("div");
+    totalBuying.className ="total-content";
+    totalBuying.innerHTML = `total: ${total} $`;
+    modalContainer.append(totalBuying);
 });
